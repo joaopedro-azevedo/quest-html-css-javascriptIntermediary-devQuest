265f2891bf68a1fc2dfd9315b1d2626be7ba8700
João Pedro <jpazevedo1998@hotmail.com>
2025-01-25 20:29:38 -0300

    add javascript

---
+++ src/js/script.js
@@ -0,0 +1,11 @@
+let inputs = document.getElementsByClassName("inputs");
+
+console.log(inputs.length);
+
+let submitButton = document.getElementsByClassName("submitButton");
+
+console.log(submitButton);
+
+function checkFields (){
+    
+}
