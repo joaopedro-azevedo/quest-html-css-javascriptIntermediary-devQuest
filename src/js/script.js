150df054c636c2a11b621782772bcf493e0c6b85
João Pedro <jpazevedo1998@hotmail.com>
2025-01-29 21:41:00 -0300

    add js

--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,36 @@
-let inputs = document.getElementsByClassName("inputs");
+/* Criar as variáveis com os inputs e com o botão submit */
+
+let inputs = document.querySelectorAll(".inputs");
 
 console.log(inputs.length);
+console.log(inputs);
+
 
 let submitButton = document.getElementsByClassName("submitButton");
 
 console.log(submitButton);
 
-function checkFields (){
-    
+
+
+/* criar um evento, onde, ao clicar no submit, passar pelo array inputs e ver se está preenchido ou não, se sim, colocar o estilo verde, se não, colocar o estilo vermelho */
+
+inputs.forEach(function() {
+    submitButton.addEventListener("click", function(){
+
+        if(inputs.reportValidity()){
+            filled();
+        } else{
+            notFilled();
+        }
+    })
+})
+
+function filled(){
+    inputs.classList.add("filled");
 }
+
+function notFilled(){
+    inputs.classList.add("notFilled");
+}
+
+
