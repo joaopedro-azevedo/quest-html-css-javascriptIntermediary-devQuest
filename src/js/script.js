8233936a7eab649baa273d2cd06ef644dae8dc4d
João Pedro <jpazevedo1998@hotmail.com>
2025-02-04 14:53:18 -0300

    ñ sei o que fazer

--- src/js/script.js
+++ src/js/script.js
@@ -1,28 +1,73 @@
 
-const inputs = document.querySelectorAll(".inputs");
 
-const submitButton = document.querySelector(".submitButton");
+ const submitButton = document.querySelector(".submitButton");
+ submitButton.addEventListener('click', function(clicked){
+  
+    clicked.preventDefault();
 
-submitButton.addEventListener('click', function(clicked){
     
-    inputs.forEach(function(input){
-        
-        input.classList.remove("notFilled", "filled");
 
-        const p = document.getElementsByClassName("hide");
-        console.log(p);
-        
-        if(inputs.values === ""){
-
-            
-
-            clicked.preventDefault();
-            input.classList.add("notFilled");
-            
-            
-        } else {
-            input.classList.add("filled");
+    function check () {
+        const inputs = document.querySelectorAll(".inputs");
+    
+     inputs.forEach(function(input){
+        let infoValidated = input.value();
+        if(){
+         input.classList.remove("notFilled", "filled");
+         const p = document.getElementsByClassName("hide");
         }
-    });
-});
+         if(infoValidated){
+             
+             input.classList.add("notFilled");
+          
+          
+         } else {
+             input.classList.add("filled");
+         }
+     });
+    }
+ });
+
+
+
+
+
+
+
+
+
+// let submitButton = document.querySelectorAll(".submitButton");
+// console.log(submitButton);
+
+// submitButton.addEventListener("click", (clicked) => {
+//     clicked.preventDefaut();
+// });
+
+// function sendForm() {
+//     check();
+// }
+
+// function check () {
+//     let infoUser = document.querySelectorAll(".inputs");
+    
+
+//     infoUser.forEach(function(input){
+//         let infoValidated = input.value();
+//         console.log(infoValidated);
+//         let putMandatoryText = input.nextElementSibling;
+        
+//         if(digitalInfo){
+//             input.classList.remove('filled', 'notFilled');
+//             putMandatoryText.textContent = "";
+//         }        
+
+//         if(infoValidated){
+//             input.classList.add("filled");
+//         } else{
+//             input.classList.add("notFilled");
+//             putMandatoryText.innerHTML = "Campo obrigatório*";
+//         }
+//     })
+// }
+
 
