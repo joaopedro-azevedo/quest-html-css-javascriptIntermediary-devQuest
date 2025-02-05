0b758623532bacefa2227fc85019fe16aa1f89d2
João Pedro de Azevedo Santos <jpazevedo1998@hotmail.com>
2025-02-05 11:07:48 -0300

    agora vai

--- src/js/script.js
+++ src/js/script.js
@@ -1,74 +1,35 @@
 
-
  let submitButton = document.querySelector(".submitButton");
+
  submitButton.addEventListener('click', function(clicked){  
     clicked.preventDefault();
+    enviarFormulario();
 });
+
     
 function enviarFormulario(){
     check();
 }
 
 function check () {
-    const inputs = document.querySelectorAll(".inputs");
+    let inputs = document.querySelectorAll(".inputs");
     
     inputs.forEach(function(input){
         let infoValidated = input.value;
+        let putMandatoryText = input.nextElementSibling;
 
         if(infoValidated){
          input.classList.remove("notFilled", "filled");
+         putMandatoryText.textContent += "";
         }
 
         if(infoValidated){ 
-            input.classList.add("notFilled");
+            input.classList.add("filled");
          } else {
-             input.classList.add("filled");
+            input.classList.add("notFilled");
+            putMandatoryText.innerHTML = `Campo obrigatório*`;
          }
     });
 }
 
-
-
-
-
-
-
-
-
-
-// const botaoDoFormulario = document.querySelectorAll(".submitButton");
-
-// botaoDoFormulario.addEventListener("click", function (clicked) {
-
-//     clicked.preventDefaut();
-
-// });
-
-
-// function enviarFormulario() {
-//     verificarSeFoiPreenchido();
-// }
-
-// function verificarSeFoiPreenchido() {
-//     let infoUser = document.querySelectorAll(".inputs");
-    
-
-//     infoUser.forEach(function(input){
-//         let infoValidated = input.values();
-//         console.log(infoValidated);
-//         let putMandatoryText = input.nextElementSibling;
-        
-//         if(infoValidated){
-//             input.classList.remove('filled', 'notFilled');
-//             putMandatoryText.textContent = "";
-//         }        
-
-//         if(infoValidated){
-//             input.classList.add("filled");
-//         } else{
-//             input.classList.add("notFilled");
-//             putMandatoryText.innerHTML = "Campo obrigatório*";
-//         }
-//     })
-// }
 
